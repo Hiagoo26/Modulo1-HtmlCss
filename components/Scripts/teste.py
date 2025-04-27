2072d612c25173c14705514fa55c85e819000f70
Hiagoo26 <hiago2608@gmail.com>
2025-04-27 18:26:17 -0300

    first commit

---
+++ components/Scripts/teste.py
@@ -0,0 +1,7 @@
+#Par ou impar
+
+n = int(input("Insira um numero: ")) # Variavel que recebe um valor INT
+if n % 2 == 0:  # Se o resto da divisão for 0
+    print("PAR") #Exibe isso
+else: #Se não 
+    print("IMPAR") #Exibe isso
